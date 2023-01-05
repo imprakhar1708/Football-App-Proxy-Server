4bfce997f1f425cfe964d5a928859a4f509e47f8
Prakhar Shrivastava <114819970+imprakhar1708@users.noreply.github.com>
2023-01-05 10:31:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const fetch = (...args) =>
@@ -13,6 +13,12 @@ var requestOptions = {
 	},
 	redirect: "follow",
 };
+app.use(
+	cors({
+		Credentials: true,
+		origin: ["3.75.158.163", "3.125.183.140", "35.157.117.28"],
+	})
+);
 app.listen(port);
 
 /****************************STANDINGS***************************/
@@ -51,71 +57,4 @@ app.get("/standings/140", async (req, res) => {
 });
 app.get("/standings/78", async (req, res) => {
 	res.json(dataSDBL);
-});
-
-/***********************LIVESCORE*************************/
-let dataL;
-const fetchDataL = async () => {
-	const apiUrl1 = `https://v3.football.api-sports.io/fixtures?league=39&season=2022&last=5`;
-	const apiUrl2 = `https://v3.football.api-sports.io/fixtures?league=2&season=2022&last=5`;
-	const res1 = await fetch(apiUrl1, requestOptions);
-	const res2 = await fetch(apiUrl2, requestOptions);
-	const r1 = await res1.json();
-	const r2 = await res2.json();
-	dataL = [r1, r2];
-};
-fetchDataL();
-
-app.get("/livescores", async (req, res) => {
-	res.json(dataL);
-});
-
-/********************************TOPSCORERS******************************/
-let dataTSPL;
-let dataTSBL;
-let dataTSCL;
-let dataTSLL;
-const fetchDataTSPL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/players/topscorers?league=39&season=2022`,
-		requestOptions
-	);
-	dataTSPL = await r.json();
-};
-const fetchDataTSCL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/players/topscorers?league=2&season=2022`,
-		requestOptions
-	);
-	dataTSCL = await r.json();
-};
-const fetchDataTSBL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/players/topscorers?league=78&season=2022`,
-		requestOptions
-	);
-	dataTSBL = await r.json();
-};
-const fetchDataTSLL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/players/topscorers?league=140&season=2022`,
-		requestOptions
-	);
-	dataTSLL = await r.json();
-};
-fetchDataTSPL();
-fetchDataTSCL();
-fetchDataTSBL();
-fetchDataTSLL();
-app.get("/topsc/39", async (req, res) => {
-	res.json(dataTSPL);
-});
-app.get("/topsc/2", async (req, res) => {
-	res.json(dataTSCL);
-});
-app.get("/topsc/140", async (req, res) => {
-	res.json(dataTSLL);
-});
-app.get("/topsc/78", async (req, res) => {
-	res.json(dataTSBL);
 });
